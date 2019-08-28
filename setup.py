0c9d84dab6a4ff01bfd55068ffcb374053ed553e
Jonas Radtke <jonas.radtke@okapiorbits.space>
2019-08-28 11:52:54 +0200

    Changes for release v201908_beta

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="okapi-python-connector",
-    version="v2019.07",
+    version="v2019.09",
     author="Jonas Radtke",
     author_email="jonas@okapiorbits.space",
     description="Package to connect to OKAPI Api",
