568cce55e145fa450fc94bfde840eb8be13f2bdd
Christopher Kebschull <christopher@okapiorbits.com>
2021-08-27 08:14:49 +0200

    Update tryout_tdm to use username and password from env

--- okapi_pkg/tryout_tdm.py
+++ okapi_pkg/tryout_tdm.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
+import os
+
 import okapi
 import sys
 
 okapi = okapi.Okapi()
 # later: space_monitor = space_monitor.SpaceMonitor()
-response = okapi.init(YOUR_LOGIN_AS_STRING, YOUR_PASSWORD_AS_STRING)
+username = os.getenv("OKAPI_USERNAME")
+password = os.getenv("OKAPI_PASSWORD")
+# You can either load the username and password from the environment or simply hardcode them here for testing purposes
+response = okapi.init(username, password)
 
 print("Initialization result status: " + str(response["status"]))
 if response["status"]["type"] == "FATAL":
@@ -15,6 +20,10 @@ response = okapi.tdms.get()
 print("TDM-Get result status: " + str(response["status"]))
 print("TDM-Get result data: " + str(response["actual_response"]))
 
+TDM_KVN_TXT_FILE_PATH_AS_STRING = None
+if TDM_KVN_TXT_FILE_PATH_AS_STRING is None:
+    print("Make sure to load a TDM (KVN) into TDM_KVN_TXT_FILE_PATH_AS_STRING.")
+    sys.exit(1)
 response = okapi.tdms.add(TDM_KVN_TXT_FILE_PATH_AS_STRING, "txt")
 
 print("TDM-Add result status: " + str(response["status"]))
