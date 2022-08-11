a9e0fb48ca0e2bccdcff8d1ee8374604c2c82c95
svenflegel <100153480+svenflegel@users.noreply.github.com>
2022-08-11 18:45:49 +0200

    Update tryout_tdm.py
    Updates URL and credentials tags to match README.md environment settings.

--- tryout_tdm.py
+++ tryout_tdm.py
@@ -9,11 +9,11 @@ from okapi_pkg import *
 # For auth info: See https://okapiorbits.space/documentation/ or contact us.
 # Standard url is: https://api.okapiorbits.com/
 load_dotenv()
-username = os.getenv("OKAPI_TEST_USERNAME")
-password = os.getenv("OKAPI_TEST_PASSWORD")
+username = os.getenv("OKAPI_USERNAME")
+password = os.getenv("OKAPI_PASSWORD")
 # You can either load the username and password from the environment or a .env file or
 # simply hardcode them here for testing purposes.
-url = os.getenv("OKAPI_TEST_URL") # "https://api.okapiorbits.com/"
+url = os.getenv("OKAPI_URL") # "https://api.okapiorbits.com/"
 okapi_login, error = okapi_init(url,
                                 username,
                                 password)
